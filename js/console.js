d5a4810bba0dd8e4884263b1d339b18a5d7490ba
chonepieceyb <34810016+chonepieceyb@users.noreply.github.com>
2019-08-24 09:10:21 +0800

    实现一些跳转的功能

---
+++ js/console.js
@@ -0,0 +1,27 @@
+
+//一些跳转功能
+//新建会议的跳转
+$(function(){
+    $("#new-activity-bottom").on('click',function (e) {
+        window.location.href="console_newMeeting.html"
+    })
+});
+//查看详情按钮的跳转
+$(function(){
+    $(".activity-card *button.btn-primary").on('click',function (e) {
+        window.location.href="show_meeting_info.html"
+    })
+});
+//编辑按钮的跳转
+$(function(){
+    $(".activity-card *button.btn-light").on('click',function (e) {
+        window.location.href="console_newMeeting.html"
+    })
+});
+
+//主页的跳蛛
+$(function () {
+    $(".activity-list-buttom").on('click',function (e) {
+        window.location.href="index.html"
+    })
+})
