d4317c15f292f9f83baca305f8c4e96a34545704
chonepieceyb <34810016+chonepieceyb@users.noreply.github.com>
2019-08-28 19:02:33 +0800

    完成了新建会议模块

--- js/console_newMeeting.js
+++ js/console_newMeeting.js
@@ -1,58 +1,7 @@
 //bootstrap fileinput
 
 
-// $(function () {
-//     //0.初始化fileinput
-//     var oFileInput = new FileInput();
-//     oFileInput.Init("uploadFile", "/api/OrderApi/ImportOrder");
-// });
-//
-// //初始化fileinput
-// var FileInput = function () {
-//     var oFile = new Object();
-//
-//     //初始化fileinput控件（第一次初始化）
-//     oFile.Init = function(ctrlName, uploadUrl) {
-//         var control = $('#' + ctrlName);
-//
-//         //初始化上传控件的样式
-//         control.fileinput({
-//             language: 'zh', //设置语言
-//             uploadUrl: uploadUrl, //上传的地址
-//             //allowedFileExtensions: ['jpg', 'gif', 'png','txt','word'],//接收的文件后缀
-//             showUpload: false, //是否显示上传按钮
-//             showCaption: false,//是否显示标题
-//             browseClass: "btn btn-primary", //按钮样式
-//             //dropZoneEnabled: false,//是否显示拖拽区域
-//             //minImageWidth: 50, //图片的最小宽度
-//             //minImageHeight: 50,//图片的最小高度
-//             //maxImageWidth: 1000,//图片的最大宽度
-//             //maxImageHeight: 1000,//图片的最大高度
-//             //maxFileSize: 0,//单位为kb，如果为0表示不限制文件大小
-//             //minFileCount: 0,
-//             maxFileCount: 10, //表示允许同时上传的最大文件个数
-//             enctype: 'multipart/form-data',
-//             validateInitialCount:true,
-//             previewFileIcon: "<i class='glyphicon glyphicon-king'></i>",
-//             msgFilesTooMany: "选择上传的文件数量({n}) 超过允许的最大数值{m}！",
-//         }    );
-//
-//         //导入文件上传完成之后的事件
-//         $("#txt_file").on("fileuploaded", function (event, data, previewId, index) {
-//             $("#myModal").modal("hide");
-//             var data = data.response.lstOrderImport;
-//             if (data == undefined) {
-//                 toastr.error('文件格式类型不正确');
-//                 return;
-//             }
-//             //1.初始化表格
-//             var oTable = new TableInit();
-//             oTable.Init(data);
-//             $("#div_startimport").show();
-//         });
-//     }
-//     return oFile;
-// };
+
 
 /**
  * @author chonepieceyb
@@ -97,9 +46,20 @@ function initFileInput(colID,url){
         maxFileSize: 0,//单位为kb，如果为0表示不限制文件大小
         minFileCount: 0,
         maxFileCount: 10, //表示允许同时上传的最大文件个数
-        validateInitialCount:true,
+        showAjaxErrorDetails:false,
+        fileActionSettings: {
+            showUpload: false,
+            showZoom: true,
+        }
 
-    })
+    });
+    $("#"+colID).on("fileuploaded",function () {
+        toastMessage("上传文件成功");
+       // window.location.href="console.html";
+    }).on("fileuploaderror",function () {
+        toastMessage("上传文件失败");
+        //window.location.href='console.html';
+    });
 }
 
 
@@ -150,23 +110,6 @@ function toastMessage(message) {
  * @usage:上传文件模块
  * @param url 上传地址
  */
-function uploadeFiles(colID,url){
-    $("#"+colID).fileinput({
-        uploadUrl:url
-    })
-
-    $("#"+colID).on("fileuploaded",function (event, data, previewId, index) {
-        toastMessage("上传文件成功");
-        window.location.href="console.html";
-    }).on("fileerror",function (event,data,msg) {
-        toastMessage("上传文件失败");
-        console("上传失败!");
-        window.location.href='console.html';
-    });
-
-    $("#"+colID).fileinput("upload");
-}
-
 
 /**
  * @author:chonepieceyb
@@ -201,7 +144,10 @@ function uploadActivityInfo(){
         success:function (data) {
             //更改 info
             toastMessage("创建会议成功，开始上传会议文件！");
-            uploadeFiles("upload-file-input",data["api"]);
+            $('#upload-file-input').on('filepreupload', function(event, dataNext, jqXHR) {
+                dataNext.extra={"act_uuid": data["uuid"]};
+            });
+            $('#upload-file-input').fileinput("upload");
             return true;
         },
         error:function () {
@@ -214,9 +160,9 @@ function uploadActivityInfo(){
 
 //浏览器加载时运行
 $(function () {
-    initFileInput("upload-file-input","/avatar");
+    initFileInput("upload-file-input","api/activity/uploadFile/");
 
-    $("#upload-activity-logo").on('change',);    //上传图片
+    $("#upload-activity-logo").on('change',uploadActivityLogo);    //上传图片
 
     //保存按钮(还没有上传文件）
     $("#activity-save-btn").on('click',uploadActivityInfo);
