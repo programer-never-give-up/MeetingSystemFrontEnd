db2f086518ec266ec011c57939b77b43be97855d
shixuezhiyi <49591807+shixuezhiyi@users.noreply.github.com>
2019-08-29 10:29:21 +0800

    1

--- js/history_attend.js
+++ js/history_attend.js
@@ -1,44 +1,66 @@
-var data=
-    {
-        "act":
-            [
-            {
-                "uuid":123,
-                "activityName":"东南大学实训宣讲会1",
 
-            },
-            {
-                "uuid":123,
-                "activityName":"东南大学实训宣讲会2",
-            },
-            {
-                "uuid":123,
-                "activityName":"东南大学实训宣讲会3",
-            }
-            ]
-    };
+
+// var data=
+//     {
+//         "act":
+//             [
+//             {
+//                 "uuid":123,
+//                 "activityName":"东南大学实训宣讲会1",
+//                 "time":{"startTime":"2019-6-8","endTime":"2019-6-9"},
+//             },
+//             {
+//                 "uuid":123,
+//                 "activityName":"东南大学实训宣讲会2",
+//                 "time":{"startTime":"2019-6-8","endTime":"2019-6-9"},
+//             },
+//             {
+//                 "uuid":123,
+//                 "activityName":"东南大学实训宣讲会3",
+//                 "time":{"startTime":"2019-6-8","endTime":"2019-6-9"},
+//             }
+//             ]
+//     };
 function Render()
 {
-    function run1()
-    {
-        for(var i=0;i<data.act.length;i++)
+    $.ajax({
+
+        url: "api/history_attend",
+        contentType: 'application/json;charset=UTF-8',
+        dataType: "json",
+        data: JSON.stringify(data),
+        type: "get",
+        async: false,
+        success: function (data)
         {
-            var x = document.getElementById("history")
-            var y1 = document.createElement("div");
-            var y2 = document.createElement("div");
-            var y4 = document.createElement("div")
-            var y5 = document.createElement("button");
-            y2.className = "card-body";
-            y1.className = "card";
-            y4.innerText = "label";
-            y4.innerText = data.act[i].activityName;
-            y5.className = "btn btn-info";
-            y5.innerText = "详情";
-            y2.appendChild(y4);
-            y2.appendChild(y5);
-            y1.appendChild(y2);
-            x.appendChild(y1);
+            for (var i = 0; i < data.act.length; i++)
+            {
+                var x = document.getElementById("history")
+                var gF = document.createElement("tr");
+                var F1 = document.createElement("td");
+                var S1 = document.createElement("a");
+                var F2 = document.createElement("td");
+                var F3 = document.createElement("td");
+                var baseUrl = "show_meeting_info";
+                var Url = baseUrl + "?id=" + data.act[i].uuid;
+                S1.setAttribute('href', Url);
+                S1.innerText = data.act[i].activityName;
+                F2.innerText = data.act[i].time.startTime;
+                F3.innerText = data.act[i].time.endTime;
+                F1.appendChild(S1);
+                gF.appendChild(F1);
+                gF.appendChild(F2);
+                gF.appendChild(F3);
+                x.appendChild(gF);
+            }
+
+        },
+        error: function () {
+
+
         }
-    }
+    });
+
+
 
 }
