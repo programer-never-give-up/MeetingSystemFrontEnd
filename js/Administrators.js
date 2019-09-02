81b8d04f17a8ad51ea3a6cd74eeb155eaf524ba0
shixuezhiyi <49591807+shixuezhiyi@users.noreply.github.com>
2019-09-02 16:50:46 +0800

    1

--- js/Administrators.js
+++ js/Administrators.js
@@ -2,7 +2,7 @@
 function Render ()
 {
     $.ajax({
-        url: "api/personal_center/history_attend/",
+        url: "api/yw/showActivityList/",
         dataType: "json",
         type: "get",
         success: function (data)
@@ -44,21 +44,14 @@ function setInfo(data_min,elementId,baseUrl)
     var F2 = document.createElement("div");
     F2.className = "item-info";
     var S2 = document.createElement("a");
-    var S4 = document.createElement("a");
     S2.setAttribute('href', Url);
-    S4.setAttribute('href', Url);
     var gS1 = document.createElement("h5");
-    var gS2 = document.createElement("button");
-    gS2.innerText = "详情";
-    gS2.className = "btn btn-info";
-    // gS2.onclick = "location=Url";
     var S3 = document.createElement("h6");
     var S5 = document.createElement("h6");
     S3.innerText = "地点：" + data_min["location"] + " 时间：" + data_min["start_time"] + "——" + data_min["end_time"];
     S5.innerText = data_min["organizer"];
     gS1.innerText = data_min["activityName"];
     S2.appendChild(gS1);
-    S4.appendChild(gS2);
     F2.appendChild(S2);
     F2.appendChild(S3);
     F2.appendChild(S5);
