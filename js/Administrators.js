4a56119034c37ccbf705db461964bed0203e43fa
Inderway <1040214708@qq.com>
2019-09-02 17:06:20 +0800

    1

--- js/Administrators.js
+++ js/Administrators.js
@@ -8,19 +8,23 @@ function Render ()
         success: function (data)
         {
             for (var i = 0; i < data["list_activity"].length; i++)
-            {
+            {	
+				console.log("for");
                 if (data["list_activity"][i]["action"] == "publish")
                 {
+					console.log("publish");
                     var baseUrl = "administrator_meeting_publish.html";
                     setInfo(data["list_activity"][i], "publishRequest", baseUrl)
                 }
                 if (data["list_activity"][i]["action"] == "modify")
                 {
+					console.log("modify");
                     var baseUrl = "administrator_meeting_modify.html";
                     setInfo(data["list_activity"][i], "modifyRequest", baseUrl)
                 }
                 if (data["list_activity"][i]["action"] == "delete")
                 {
+					console.log("delete");
                     var baseUrl = "administrator_meeting_delete.html";
                     setInfo(data["list_activity"][i], "deleteRequest", baseUrl)
                 }
@@ -50,12 +54,11 @@ function setInfo(data_min,elementId,baseUrl)
     var S5 = document.createElement("h6");
     S3.innerText = "地点：" + data_min["location"] + " 时间：" + data_min["start_time"] + "——" + data_min["end_time"];
     S5.innerText = data_min["organizer"];
-    gS1.innerText = data_min["activityName"];
+    gS1.innerText = data_min["name_act"];
     S2.appendChild(gS1);
     F2.appendChild(S2);
     F2.appendChild(S3);
     F2.appendChild(S5);
-    F2.appendChild(S4);
     gF.appendChild(F1);
     gF.appendChild(F2);
     var x = document.getElementById(elementId);
