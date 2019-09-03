f0f532e66249a353eee3f10151ca11fe67aebbd9
shixuezhiyi <49591807+shixuezhiyi@users.noreply.github.com>
2019-09-03 09:57:49 +0800

    Update Administrators.js

--- js/Administrators.js
+++ js/Administrators.js
@@ -65,32 +65,32 @@ function setInfo(data_min,elementId,baseUrl)
     var buttonDisagree=document.createElement("button");
     buttonDisagree.className="btn btn-danger";
     buttonAgree.innerText="同意";
-    buttonDisagree.onclick="disagreeClick(data_min)";
-    buttonAgree.onclick="agreeClick(data_min)";
+    buttonAgree.onclick=function ()
+    {
+        var x;
+        var r=confirm("确定同意吗!");
+        if (r==true)
+        {
+            agree(data_min);
+        }
+        
+    };
+    buttonDisagree.onclick=function()
+    {
+        var x;
+        var r=confirm("确定拒绝吗!");
+        if (r==true)
+        {
+            disagree(data_min);
+        }
+    }
     buttonDisagree.innerText="拒绝";
     F2.appendChild(buttonAgree);
     F2.appendChild(buttonDisagree);
     var x = document.getElementById(elementId);
     x.appendChild(gF);
 }
-function agreeClick(dataSent)
-{
-    var x;
-    var r=confirm("确定同意吗!");
-    if (r==true)
-    {
-        agree(dataSent);
-    }
-}
-function disagreeClick(dataSent)
-{
-    var x;
-    var r=confirm("确定拒绝吗!");
-    if (r==true)
-    {
-        disagree(dataSent);
-    }
-}
+
 function agree(dataSent)
 {
     if(dataSent["action"]=="publish")
